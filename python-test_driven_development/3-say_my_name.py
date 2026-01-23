eafa2501fa0502fe4823179e3cadf48b86b8b82d
Aythann <aythancristovao@gmail.com>
2026-01-23 13:06:27 +0100

    Update say_my_name behavior and doctest

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,9 +5,20 @@ This module provides a function that prints a full name.
 
 
 def say_my_name(first_name, last_name=""):
-    """Print My name is <first_name> <last_name>."""
+    """
+    Print "My name is <first_name> <last_name>".
+
+    Raises:
+        TypeError: if first_name or last_name is not a string
+        ValueError: if first_name is an empty string
+    """
+    if first_name == "":
+        raise ValueError("first_name must be a string")
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+
     print("My name is {} {}".format(first_name, last_name))
