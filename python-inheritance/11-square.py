cf15a4adc39d8ee450b4e019f44e12c1ab1ee763
Aythann <aythancristovao@gmail.com>
2026-02-03 10:55:14 +0100

    Improve Square string representation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a Square class that inherits from Rectangle."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square with validated size."""
+
+    def __init__(self, size):
+        """Initializes a square with validated size."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Returns the string representation of the square."""
+        return f"[Square] {self.__size}/{self.__size}"
