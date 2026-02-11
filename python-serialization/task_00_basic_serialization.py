a0e5da3c3a322e1558b52c844dabaae0693b141a
Aythann <aythancristovao@gmail.com>
2026-02-11 13:01:57 +0100

    Add basic JSON serialization and deserialization

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serialize a Python dictionary to a JSON file.
+
+    :param data: Python dictionary to serialize
+    :param filename: Output JSON file name
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file)
+
+
+def load_and_deserialize(filename):
+    """
+    Load and deserialize a JSON file into a Python dictionary.
+
+    :param filename: Input JSON file name
+    :return: Python dictionary
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
