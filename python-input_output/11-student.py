ef01491a52cef9c63b7ecb10b2c0d442eea904ef
Aythann <aythancristovao@gmail.com>
2026-02-11 02:42:57 +0100

    Add Student serialization and reload methods

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with JSON serialization and reloading."""
+
+
+class Student:
+    """Class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes listed in attrs
+        are included in the returned dictionary.
+        """
+        if isinstance(attrs, list):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance from a dictionary."""
+        for key, value in json.items():
+            setattr(self, key, value)
