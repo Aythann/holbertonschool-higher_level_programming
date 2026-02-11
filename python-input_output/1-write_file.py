8a70385df56b5380b0d4c17b74588d6a89d7a2d3
Aythann <aythancristovao@gmail.com>
2026-02-11 12:41:35 +0100

    fix file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
-
-"""This module defines a function that write inside of a file."""
+"""
+This module defines a function that writes text into a file.
+"""
 
 
 def write_file(filename="", text=""):
     """
-	Function that write text inside of filename
-	"""
+    Function that writes text inside of filename.
+    """
     with open(filename, 'w') as file:
-        len_file = file.write(text)
-        return len_file
+        return file.write(text)
