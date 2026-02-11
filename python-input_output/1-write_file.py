a8218801f6a9bd8f915adf70c805ef0334f8c199
Aythann <aythancristovao@gmail.com>
2026-02-11 12:40:09 +0100

    fix file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a function that writes a string to a UTF-8 text file."""
+
+
+"""This module defines a function that write inside of a file."""
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    """
+	Function that write text inside of filename
+	"""
     with open(filename, 'w') as file:
         len_file = file.write(text)
         return len_file
