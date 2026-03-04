c68db54db9a8292cb7546068957b0e2e5f9d2732
Aythann <aythancristovao@gmail.com>
2026-03-04 06:54:45 +0100

    add 9-full_creation.sql file

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Writes a script that creates second_table and inserts several rows into it
+CREATE TABLE IF NOT EXISTS second_table (
+  id INT,
+  name VARCHAR(256),
+  score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES
+  (1, 'John', 10),
+  (2, 'Alex', 3),
+  (3, 'Bob', 14),
+  (4, 'George', 8);
