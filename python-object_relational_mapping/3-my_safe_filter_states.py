fa6fae2c6161d3df94ea0518db0ed795a3628e30
Aythann <aythancristovao@gmail.com>
2026-03-04 07:18:22 +0100

    add 3-my_safe_filter_states.py file

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Displays states where name matches the argument (injection-safe)."""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+        charset="utf8",
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sys.argv[4],))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
