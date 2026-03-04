63ab11f3e58e6876dee8c1cfece076d86414935d
Aythann <aythancristovao@gmail.com>
2026-03-04 07:07:20 +0100

    add 5-unique_id.sql file

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Writes a script that creates the table unique_id with a unique id field defaulting to 1
+CREATE TABLE IF NOT EXISTS unique_id (
+  id INT DEFAULT 1 UNIQUE,
+  name VARCHAR(256)
+);
