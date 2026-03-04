7eb27be59d90a1a44ad1b8905900d33f89f85952
Aythann <aythancristovao@gmail.com>
2026-03-04 07:31:21 +0100

    fix pycodestyle for 2-my_filter_states.py file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,10 @@ if __name__ == "__main__":
         charset="utf8",
     )
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
+    query = (
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+        .format(sys.argv[4])
+    )
     cur.execute(query)
     for row in cur.fetchall():
         print(row)
