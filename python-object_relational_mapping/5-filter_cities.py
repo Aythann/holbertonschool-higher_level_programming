a637e1f48d5dea9d878da51fa91c77e1701ddfd4
Aythann <aythancristovao@gmail.com>
2026-03-04 07:46:28 +0100

    fix output for 5-filter_cities.py file

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -15,17 +15,13 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        """
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-        """,
+        "SELECT cities.name FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC",
         (sys.argv[4],),
     )
     rows = cur.fetchall()
-    if rows:
-        print(", ".join([r[0] for r in rows]))
+    print(", ".join([row[0] for row in rows]))
     cur.close()
     db.close()
