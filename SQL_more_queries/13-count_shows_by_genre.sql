368887d0a7b9e094c6261a62988970bd41671c76
Aythann <aythancristovao@gmail.com>
2026-03-04 07:07:20 +0100

    add 13-count_shows_by_genre.sql file

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Writes a script that lists genres and the number of shows linked to each (genre - number_of_shows)
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id, tv_genres.name
+HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY number_of_shows DESC;
