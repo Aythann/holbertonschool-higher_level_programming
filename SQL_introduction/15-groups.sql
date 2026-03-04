4c625c08d6d8cd186afcb1232db109b606251993
Aythann <aythancristovao@gmail.com>
2026-03-04 06:54:45 +0100

    add 15-groups.sql file

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- Writes a script that displays the number of records for each score ordered by count descending
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
