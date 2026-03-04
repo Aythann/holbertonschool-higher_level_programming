de3cb1e63deab11646ffcd2b47df1a16be37c96c
Aythann <aythancristovao@gmail.com>
2026-03-04 07:18:22 +0100

    add 14-model_city_fetch_by_state.py file

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Prints all City objects from the database, ordered by cities.id."""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True,
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    rows = (
+        session.query(State.name, City.id, City.name)
+        .join(City, City.state_id == State.id)
+        .order_by(City.id)
+        .all()
+    )
+
+    for state_name, city_id, city_name in rows:
+        print("{}: ({}) {}".format(state_name, city_id, city_name))
+
+    session.close()
