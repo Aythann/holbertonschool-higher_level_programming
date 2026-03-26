8c3173fdb5dc9364460d3a58f828dab40b118785
Aythann <aythancristovao@gmail.com>
2026-03-26 10:02:37 +0100

    fix output code

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,32 +1,34 @@
 from flask import Flask, render_template
 import json
+import os
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route('/about')
 def about():
     return render_template('about.html')
-
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-
 @app.route('/items')
 def items():
-    with open('items.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
+    json_path = os.path.join(app.root_path, 'items.json')
 
-    items_list = data.get('items', [])
+    try:
+        with open(json_path, 'r') as f:
+            data = json.load(f)
+            items_list = data.get("items", [])
+    except Exception as e:
+        print(f"Error reading JSON file: {e}")
+        items_list = []
+
     return render_template('items.html', items=items_list)
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
